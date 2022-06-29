629a1f1466c95393c5ad4e87288c96ca07905d08
ClemDSC <clemence.p14@gmail.com>
2022-06-29 21:12:10 +0200

    we can get one or all project, we can update a project and we can delete it

--- backend/src/models/ProjectModel.js
+++ backend/src/models/ProjectModel.js
@@ -12,4 +12,39 @@ const createOne = async (project) => {
   }
 };
 
-module.exports = { createOne };
+const getAll = async () => {
+  try {
+    return await prisma.project.findMany();
+  } finally {
+    await prisma.$disconnect();
+  }
+};
+
+const getOne = async (id) => {
+  try {
+    return await prisma.project.findUnique({ where: { id: parseInt(id, 10) } });
+  } finally {
+    await prisma.$disconnect();
+  }
+};
+
+const editOne = async (id, data) => {
+  try {
+    return await prisma.project.update({
+      where: { id: parseInt(id, 10) },
+      data,
+    });
+  } finally {
+    await prisma.$disconnect();
+  }
+};
+
+const deleteOne = async (id) => {
+  try {
+    return await prisma.project.delete({ where: { id: parseInt(id, 10) } });
+  } finally {
+    await prisma.$disconnect();
+  }
+};
+
+module.exports = { createOne, getAll, getOne, editOne, deleteOne };
--- backend/src/router.js
+++ backend/src/router.js
@@ -10,6 +10,10 @@ router.post("/users", UserController.createOne);
 router.put("/users/:id", UserController.editOne);
 router.delete("/users/:id", UserController.deleteOne);
 
+router.get("/projects", ProjectController.getAll);
+router.get("/projects/:id", ProjectController.getOne);
 router.post("/users/:userId/projects", ProjectController.createOne);
+router.put("/projects/:id", ProjectController.editOne);
+router.delete("/projects/:id", ProjectController.deleteOne);
 
 module.exports = router;
