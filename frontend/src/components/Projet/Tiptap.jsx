a4c9d869c3ed76f9ad1e87e323ef6e80dc51cc91
Julie Combeau <combeau.julie@gmail.com>
2022-06-30 01:05:55 +0200

    fix before pull

--- frontend/src/components/Projet/Tiptap.jsx
+++ frontend/src/components/Projet/Tiptap.jsx
@@ -1,6 +1,7 @@
+/* eslint-disable react/button-has-type */
 /* eslint-disable react/prop-types */
 import { useEditor, EditorContent } from "@tiptap/react";
-import { Button, Flex } from "@chakra-ui/react";
+// import { Flex } from "@chakra-ui/react";
 import StarterKit from "@tiptap/starter-kit";
 
 function MenuBar({ editor }) {
@@ -9,79 +10,112 @@ function MenuBar({ editor }) {
   }
 
   return (
-    <Flex w="fit-content" flexWrap="wrap">
-      <Button onClick={() => editor.chain().focus().toggleBold().run()}>
+    <>
+      <button
+        onClick={() => editor.chain().focus().toggleBold().run()}
+        className={editor.isActive("bold") ? "is-active" : ""}
+      >
         bold
-      </Button>
-      <Button onClick={() => editor.chain().focus().toggleItalic().run()}>
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleItalic().run()}
+        className={editor.isActive("italic") ? "is-active" : ""}
+      >
         italic
-      </Button>
-      <Button onClick={() => editor.chain().focus().toggleStrike().run()}>
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleStrike().run()}
+        className={editor.isActive("strike") ? "is-active" : ""}
+      >
         strike
-      </Button>
-      <Button onClick={() => editor.chain().focus().toggleCode().run()}>
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleCode().run()}
+        className={editor.isActive("code") ? "is-active" : ""}
+      >
         code
-      </Button>
-      <Button onClick={() => editor.chain().focus().unsetAllMarks().run()}>
+      </button>
+      <button onClick={() => editor.chain().focus().unsetAllMarks().run()}>
         clear marks
-      </Button>
-      <Button onClick={() => editor.chain().focus().clearNodes().run()}>
+      </button>
+      <button onClick={() => editor.chain().focus().clearNodes().run()}>
         clear nodes
-      </Button>
-      <Button onClick={() => editor.chain().focus().setParagraph().run()}>
+      </button>
+      <button
+        onClick={() => editor.chain().focus().setParagraph().run()}
+        className={editor.isActive("paragraph") ? "is-active" : ""}
+      >
         paragraph
-      </Button>
-      <Button
+      </button>
+      <button
         onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
+        className={editor.isActive("heading", { level: 1 }) ? "is-active" : ""}
       >
         h1
-      </Button>
-      <Button
+      </button>
+      <button
         onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
+        className={editor.isActive("heading", { level: 2 }) ? "is-active" : ""}
       >
         h2
-      </Button>
-      <Button
+      </button>
+      <button
         onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
+        className={editor.isActive("heading", { level: 3 }) ? "is-active" : ""}
       >
         h3
-      </Button>
-      <Button
+      </button>
+      <button
         onClick={() => editor.chain().focus().toggleHeading({ level: 4 }).run()}
+        className={editor.isActive("heading", { level: 4 }) ? "is-active" : ""}
       >
         h4
-      </Button>
-      <Button
+      </button>
+      <button
         onClick={() => editor.chain().focus().toggleHeading({ level: 5 }).run()}
+        className={editor.isActive("heading", { level: 5 }) ? "is-active" : ""}
       >
         h5
-      </Button>
-      <Button
+      </button>
+      <button
         onClick={() => editor.chain().focus().toggleHeading({ level: 6 }).run()}
+        className={editor.isActive("heading", { level: 6 }) ? "is-active" : ""}
       >
         h6
-      </Button>
-      <Button onClick={() => editor.chain().focus().toggleBulletList().run()}>
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleBulletList().run()}
+        className={editor.isActive("bulletList") ? "is-active" : ""}
+      >
         bullet list
-      </Button>
-      <Button onClick={() => editor.chain().focus().toggleOrderedList().run()}>
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleOrderedList().run()}
+        className={editor.isActive("orderedList") ? "is-active" : ""}
+      >
         ordered list
-      </Button>
-      <Button onClick={() => editor.chain().focus().toggleCodeBlock().run()}>
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleCodeBlock().run()}
+        className={editor.isActive("codeBlock") ? "is-active" : ""}
+      >
         code block
-      </Button>
-      <Button onClick={() => editor.chain().focus().toggleBlockquote().run()}>
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleBlockquote().run()}
+        className={editor.isActive("blockquote") ? "is-active" : ""}
+      >
         blockquote
-      </Button>
-      <Button onClick={() => editor.chain().focus().setHorizontalRule().run()}>
+      </button>
+      <button onClick={() => editor.chain().focus().setHorizontalRule().run()}>
         horizontal rule
-      </Button>
-      <Button onClick={() => editor.chain().focus().setHardBreak().run()}>
+      </button>
+      <button onClick={() => editor.chain().focus().setHardBreak().run()}>
         hard break
-      </Button>
-      <Button onClick={() => editor.chain().focus().undo().run()}>undo</Button>
-      <Button onClick={() => editor.chain().focus().redo().run()}>redo</Button>
-    </Flex>
+      </button>
+      <button onClick={() => editor.chain().focus().undo().run()}>undo</button>
+      <button onClick={() => editor.chain().focus().redo().run()}>redo</button>
+    </>
   );
 }
 
@@ -89,12 +123,34 @@ function Tiptap() {
   const editor = useEditor({
     extensions: [StarterKit],
     content: `
-    <Text fontSize="2xl" align="left">
-      Détail du projet
-    </Text>
-    <Text fontSize="2xl" align="left">
-    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti illum sint atque reiciendis dolor laborum quia possimus illo ab sapiente, modi qui eaque suscipit nemo. Nemo, earum. Quos, suscipit molestiae.
-    </Text>
+    <h2>
+        Hi there,
+      </h2>
+      <p>
+        this is a <em>basic</em> example of <strong>tiptap</strong>. Sure, there are all kind of basic text styles you’d probably expect from a text editor. But wait until you see the lists:
+      </p>
+      <ul>
+        <li>
+          That’s a bullet list with one …
+        </li>
+        <li>
+          … or two list items.
+        </li>
+      </ul>
+      <p>
+        Isn’t that great? And all of that is editable. But wait, there’s more. Let’s try a code block:
+      </p>
+      <pre><code class="language-css">body {
+  display: none;
+}</code></pre>
+      <p>
+        I know, I know, this is impressive. It’s only the tip of the iceberg though. Give it a try and click a little bit around. Don’t forget to check the other examples too.
+      </p>
+      <blockquote>
+        Wow, that’s amazing. Good work, boy! 👏
+        <br />
+        — Mom
+      </blockquote>
     `,
   });
 
