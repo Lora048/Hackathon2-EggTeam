005886ab479c7b57c867755b55c91eeeb4aa9a22
Julie Combeau <combeau.julie@gmail.com>
2022-06-29 17:18:15 +0200

    initialisation project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,17 @@
-import Home from "@pages/Home";
+import { Route, Routes } from "react-router-dom";
+import Projet from "./pages/Projet";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Home />
-      <p>coucou</p>
+      <Routes>
+        {/* <Route path="/" element={<Inscription />} /> */}
+        {/* <Route path="/acceuil" element={<Acceuil />} /> */}
+        <Route path="/projet" element={<Projet />} />
+        {/* <Route path="/profil" element={<Profil />} /> */}
+      </Routes>
     </div>
   );
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,22 @@
 import React from "react";
+
 import ReactDOM from "react-dom/client";
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop";
 
 import App from "./App";
+import theme from "./themes/theme";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <ScrollToTop />
+      <ChakraProvider theme={theme}>
+        <App />
+      </ChakraProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
