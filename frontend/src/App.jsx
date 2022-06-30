1c1c34e8783dac7d1df8942d5f7326de891b7b21
MARIE SERRADORI <marie.serradori@gmail.com>
2022-06-30 21:23:52 +0200

    fixed conflicts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,9 @@
 import { Route, Routes } from "react-router-dom";
 
 import "./App.css";
-import Accueil from "./pages/Accueil";
+
 import Projet from "./pages/Projet";
+import Accueil from "./pages/Accueil";
 
 function App() {
   return (
@@ -12,7 +13,6 @@ function App() {
         <Route path="/accueil/:userId" element={<Accueil />} />
         <Route path="/projet" element={<Projet />} />
         <Route path="/accueil" element={<Accueil />} />
-        <Route path="/accueil/:userid" element={<Accueil />} />
         <Route path="/users/:userId/projet/:projectId" element={<Projet />} />
         {/* <Route path="/profil" element={<Profil />} /> */}
       </Routes>
