bce67d012b18a7376d1e961a3c7903b3f3d01319
Sylvain LEGUAY <leguay.sylvain40@gmail.com>
2022-06-30 15:17:50 +0200

    Co-authored-by: ClemDSC <ClemDSC@users.noreply.github.com> Co-authored-by: MarieSerra <Pyramate@users.noreply.github.com>

--- frontend/src/components/Projet/Commentaires.jsx
+++ frontend/src/components/Projet/Commentaires.jsx
@@ -1,7 +1,26 @@
 import { Flex, Text } from "@chakra-ui/react";
 import { ReactTinyLink } from "react-tiny-link";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function Commentaires() {
+  const [contents, setContents] = useState([]);
+  const [replies, setReplies] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5001/api/users/1/projects/1/comments")
+      .then((res) => setContents(res.data));
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get(
+        `http://localhost:5001/api/users/1/projects/1/comments/${contents.id}/reply`
+      )
+      .then((res) => setReplies(res.data));
+  }, []);
+
   return (
     <Flex>
       <Flex p="2rem" flexDir="column" w="100%" gap="4">
@@ -15,6 +34,14 @@ export default function Commentaires() {
           minLine={1}
           url="https://www.figma.com/file/KTFPuhOkABSq8lPWepVlqh/Hackathon2-x-EggTeam?node-id=34%3A6"
         />
+        {contents &&
+          contents.map(
+            (content) =>
+              <Flex key={content.id}>{content.content}</Flex> &&
+              replies.map((reply) => (
+                <Flex key={reply.id}>{reply.content}</Flex>
+              ))
+          )}
       </Flex>
     </Flex>
   );
