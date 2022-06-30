096621e359ba56c8ed73147d5c19b223713648eb
Julie Combeau <combeau.julie@gmail.com>
2022-06-30 17:12:39 +0200

    star for chef of project

--- frontend/src/components/Projet/Contributeurs.jsx
+++ frontend/src/components/Projet/Contributeurs.jsx
@@ -9,6 +9,7 @@ import {
   Thead,
   Tr,
 } from "@chakra-ui/react";
+import { StarIcon } from "@chakra-ui/icons";
 
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
@@ -17,7 +18,7 @@ import axios from "axios";
 export default function Contributeurs() {
   const [contributors, setContributors] = useState();
 
-  const { projectId } = useParams();
+  const { userId, projectId } = useParams();
 
   const getAllCollaborators = () => {
     axios
@@ -28,6 +29,7 @@ export default function Contributeurs() {
   };
 
   useEffect(() => getAllCollaborators(), []);
+
   return (
     <Flex
       direction="column"
@@ -43,25 +45,25 @@ export default function Contributeurs() {
         mb="10px"
         boxShadow="0px 40px 58px -20px rgba(112, 144, 176, 0.26)"
       >
-        <Text fontSize="xl" fontWeight="600">
-          Top Creators
+        <Text fontSize="2xl" fontWeight="600" color="navy.700">
+          Contributeurs du projet
         </Text>
       </Flex>
       <Table variant="simple" color="gray.500">
         <Thead>
           <Tr>
             <Th pe="10px" borderColor="transparent">
-              <Text fontSize={{ sm: "10px", lg: "12px" }} color="gray.400">
-                Contributeurs
+              <Text fontSize="md" color="gray.400">
+                Nom Prénom
               </Text>
             </Th>
             <Th pe="10px" borderColor="transparent">
-              <Text fontSize={{ sm: "10px", lg: "12px" }} color="gray.400">
+              <Text fontSize="md" color="gray.400">
                 Job
               </Text>
             </Th>
             <Th pe="10px" borderColor="transparent">
-              <Text fontSize={{ sm: "10px", lg: "12px" }} color="gray.400">
+              <Text fontSize="md" color="gray.400">
                 Agence
               </Text>
             </Th>
@@ -72,36 +74,48 @@ export default function Contributeurs() {
             contributors.map((contributor) => (
               <Tr key={contributor.fk_participation_user_project_userId.id}>
                 <Td>
-                  <Flex align="left" justifyContent="space-around" w="50%">
+                  <Flex
+                    align="left"
+                    justifyContent="space-around"
+                    w="fit-content"
+                    alignItems="center"
+                  >
+                    {parseInt(userId, 10) === contributor.userId ? (
+                      <StarIcon w={7} h={7} color="navy.500" mr="0.5rem" />
+                    ) : (
+                      <StarIcon w={7} h={7} color="white" mr="0.5rem" />
+                    )}
                     <Avatar
                       src={
                         contributor.fk_participation_user_project_userId.picture
                       }
-                      w="30px"
-                      h="30px"
+                      w="45px"
+                      h="45px"
                       me="8px"
                     />
-                    <Text fontSize="sm" fontWeight="600">
-                      {
-                        contributor.fk_participation_user_project_userId
-                          .firstname
-                      }
-                    </Text>
-                    <Text>
-                      {
-                        contributor.fk_participation_user_project_userId
-                          .lastname
-                      }
-                    </Text>
+                    <Flex gap="2" alignItems="center">
+                      <Text fontSize="md" fontWeight="600">
+                        {
+                          contributor.fk_participation_user_project_userId
+                            .firstname
+                        }
+                      </Text>
+                      <Text fontSize="md" fontWeight="600">
+                        {
+                          contributor.fk_participation_user_project_userId
+                            .lastname
+                        }
+                      </Text>
+                    </Flex>
                   </Flex>
                 </Td>
                 <Td>
-                  <Text fontSize="sm" fontWeight="500">
+                  <Text fontSize="md" fontWeight="500">
                     {contributor.fk_participation_user_project_userId.jobPost}
                   </Text>
                 </Td>
                 <Td>
-                  <Text fontSize="sm" fontWeight="500">
+                  <Text fontSize="md" fontWeight="500">
                     {contributor.fk_participation_user_project_userId.agency}
                   </Text>
                 </Td>
