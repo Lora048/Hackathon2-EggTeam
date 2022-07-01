2f971df273299b411370424bf985fa5312777823
Julie Combeau <combeau.julie@gmail.com>
2022-07-01 10:27:02 +0200

    design page inscription

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 
 import "./App.css";
 
+import Inscription from "./pages/Inscription";
 import Projet from "./pages/Projet";
 import Accueil from "./pages/Accueil";
 import Profil from "./pages/Profil";
@@ -10,7 +11,7 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        {/* <Route path="/" element={<Inscription />} /> */}
+        <Route path="/" element={<Inscription />} />
         <Route path="/accueil/:userId" element={<Accueil />} />
         {/* <Route path="/projet" element={<Projet />} /> */}
         {/* <Route path="/accueil" element={<Accueil />} /> */}
